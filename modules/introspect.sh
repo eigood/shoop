a30551955fec65e735d47d32d5fded8458aa1734
Joey Hess <joeyh@joeyh.name>
2000-01-27 11:08:45 +0000

    indented

--- modules/introspect.sh
+++ modules/introspect.sh
@@ -6,27 +6,27 @@
 # Smell the Java. GPL copyright 2000 by Adam Heath <doogie@debian.org>
 
 BASE . introspect : '
-local OBJNAME=$1 DEFINES A DISPLAYOBJ;
-shift;
-if [ "$2" ];then
-	DISPLAYOBJ=$2;
-else
-	DISPLAYOBJ=$OBJNAME;
-fi;
-eval DEFINES=\$_shoopdefines_$OBJNAME;
-for A in $DEFINES; do
-	if eval [ -z \"\$_shoopseen_$A\" ];then
-		eval echo "$DISPLAYOBJ\($OBJNAME\): $A is \$_shooptype_${OBJNAME}_$A";
-		eval _shoopseen_$A="1";
-	fi
-done;
-if [ "$1" = resolve ];then
-	eval local PARENT=\$_shoopparent_$OBJNAME;
-	for P in $PARENT;do
-		$P . introspect resolve $DISPLAYOBJ;
+	local OBJNAME=$1 DEFINES A DISPLAYOBJ;
+	shift;
+	if [ "$2" ];then
+		DISPLAYOBJ=$2;
+	else
+		DISPLAYOBJ=$OBJNAME;
+	fi;
+	eval DEFINES=\$_shoopdefines_$OBJNAME;
+	for A in $DEFINES; do
+		if eval [ -z \"\$_shoopseen_$A\" ];then
+			eval echo "$DISPLAYOBJ\($OBJNAME\): $A is \$_shooptype_${OBJNAME}_$A";
+			eval _shoopseen_$A="1";
+		fi
+	done;
+	if [ "$1" = resolve ];then
+		eval local PARENT=\$_shoopparent_$OBJNAME;
+		for P in $PARENT;do
+			$P . introspect resolve $DISPLAYOBJ;
+		done
+	fi;
+	for A in $DEFINES; do
+		eval unset _shoopseen_$A;
 	done
-fi;
-for A in $DEFINES; do
-	eval unset _shoopseen_$A;
-done
 '
