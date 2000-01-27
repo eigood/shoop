83aecd05f176e4f4a89ecdd9aacaea612da64051
Joey Hess <joeyh@joeyh.name>
2000-01-27 08:43:05 +0000

    Mods from doogie: introspection, multiple inheritance, better knowledge about when methods are not defined, and a big slowdown, which is being corrected.

--- shoop.example.sh
+++ shoop.example.sh
@@ -1,9 +1,16 @@
 . shoop.sh
 BASE . counter = 10
 BASE . count : '$1 . counter = $(expr $($1 . counter) + 1)'
+BASE . test = 20
 BASE . count
 BASE . new FOO
+BASE . new BAR
+BAR . parent A B C D FOO
 FOO . counter
 FOO . counter = 1
 FOO . count
+BAR . test
 BASE . count
+BAR . introspect
+FOO . introspect resolve
+BASE . introspect
--- shoop.sh
+++ shoop.sh
@@ -10,19 +10,30 @@ _shoop () {
 	if [ "$1" = = ]; then
 		# Set value.
 		shift 1
-		eval "_shoop_${TRUEOBJ}_${METH} () { echo $@; }"
+		eval "_shoop_${TRUEOBJ}_$METH () { echo $@; }"
+		eval "_shooptype_${TRUEOBJ}_$METH=variable"
+		eval "_shoopdefines_$TRUEOBJ=\$(echo \$_shoopdefines_$TRUEOBJ $METH|tr ' ' '\n'|sort|uniq)"
 		echo $@
 	elif [ "$1" = : ]; then
 		# Set method.
 		shift 1
-		eval "_shoop_${TRUEOBJ}_${METH} () { $@; }"
-	elif ! eval _shoop_${TRYOBJ}_$METH $TRUEOBJ $@ 2>/dev/null; then
-		if [ "$(_shoop_${TRYOBJ}_parent 2>/dev/null)" ]; then
-			_shoop $TRUEOBJ $(_shoop_${TRYOBJ}_parent) $METH $@
-		else
-			echo "No such method, \"$METH\"" >&2
-			return 1
+		eval "_shoop_${TRUEOBJ}_$METH () { $@; }"
+		eval "_shooptype_${TRUEOBJ}_$METH=method"
+		eval "_shoopdefines_$TRUEOBJ=\$(echo \$_shoopdefines_$TRUEOBJ $METH|tr ' ' '\n'|sort|uniq)"
+	elif eval [ "\$_shooptype_${TRYOBJ}_$METH" ]; then # Bwa ha ha.
+		eval _shoop_${TRYOBJ}_$METH $TRUEOBJ \"\$@\";
+	else
+		eval local PARENTS=\$_shoopparent_$TRYOBJ
+		if [ "$PARENTS" ]; then
+			local P
+			for P in $PARENTS;do
+				if _shoop $TRUEOBJ $P $METH $@ 2>/dev/null; then
+					return 0
+				fi
+			done
 		fi
+		echo "\"$METH\" is undefined." >&2
+		return 1
 	fi
 }
 
@@ -35,8 +46,41 @@ _shoop_BASE_new () {
 	
 	eval "$OBJNAME () { shift; _shoop $OBJNAME $OBJNAME \$@; }"
 	if [ "$PARENT" ]; then
-		eval "_shoop_${OBJNAME}_parent () { echo $PARENT; }"
+		eval "_shoopparent_$OBJNAME=$PARENT"
 	fi
 }
+_shoopdefines_BASE=new
+_shooptype_BASE_new=method
 # Make the base object.
 _shoop_BASE_new '' BASE
+BASE . parent : '
+local OBJNAME=$1;
+shift;
+eval _shoopparent_$OBJNAME=\"$@\"
+'
+BASE . introspect : '
+local OBJNAME=$1 DEFINES TYPE A DISPLAYOBJ;
+shift;
+if [ "$2" ];then
+	DISPLAYOBJ=$2;
+else
+	DISPLAYOBJ=$OBJNAME;
+fi;
+eval DEFINES=\$_shoopdefines_$OBJNAME;
+for A in $DEFINES; do
+	if ! echo $_shoop_introspect_seen | tr " " "\n" | grep -q "^$A$";then
+		eval TYPE=\$_shooptype_${OBJNAME}_$A;
+		echo "$DISPLAYOBJ: $A is $TYPE";
+		_shoop_introspect_seen="$_shoop_introspect_seen $A";
+	fi
+done;
+if [ "$1" = resolve ];then
+	eval local PARENT=\$_shoopparent_$OBJNAME;
+	for P in $PARENT;do
+		$P . introspect resolve $DISPLAYOBJ;
+	done
+fi;
+for A in $DEFINES; do
+	_shoop_introspect_seen=$(echo $_shoop_introspect_seen | tr " " "\n" | grep -v "^$A$");
+done
+'
