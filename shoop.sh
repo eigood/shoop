de0687d9e1560c0ed1d24789d26975f1463327a8
Adam Heath <adam@brainfood.com>
2000-01-29 04:08:46 +0000

    Removed blank line.

--- shoop.sh
+++ shoop.sh
@@ -12,7 +12,6 @@ _shoop () {
 			eval [ -z \"\$_shooptype_$TRYMETH\" ]; then
 				eval "_shoopdefines_$TRUEOBJ=\"\$_shoopdefines_$TRUEOBJ $METH\""
 		fi
-		
 		if [ "$1" = = ]; then
 			shift
 			eval "_shoop_$TRUEMETH='echo -n $@'
