570ff74894374bdb702c51bd112063ddc6dc30b6
Adam Heath <adam@brainfood.com>
2000-01-29 09:07:13 +0000

    Add a variable 'usepath' to OBJECT, which use will look for external modules in.  The default value for this is '$SHOOPPATH:.'

--- modules/use.sh
+++ modules/use.sh
@@ -1,19 +1,26 @@
 #!/bin/sh
 #                                                                               
 # Use module for shoop. Expands the OBJECT class with a method named use, 
-# use, that can import shoop modules, and prevents multiple difinitions
-# from happenning.
+# that can import shoop modules, and prevents multiple definitions from
+# happenning.  It searches in the path defined in $THIS . usepath.
 # 
 # Example: OBJECT . use serialize destroy
 #                                                                               
 # GPL copyright 2000 by Adam Heath <doogie@debian.org>
+OBJECT . usepath = $SHOOPPATH:. > /dev/null
+
 IFS=" " OBJECT . use : '
-	local A;
+	local A B;
 	for A in "$@"; do
 		if eval [ -z \"\$_shoopuse_$A\" ]; then
-			. $A.sh;
-			_shoopuse_="$_shoopuse_ $A";
-			eval "_shoopuse_$A=1";
+			for B in $(IFS=":" $THIS . usepath);do
+				if [ -e $B/$A.sh ]; then
+					. $B/$A.sh;
+					_shoopuse_="$_shoopuse_ $A";
+					eval "_shoopuse_$A=1";
+					break;
+				fi;
+			done;
 		fi;
 	done
 '
