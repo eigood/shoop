20a90d6c47fd3071859f0ef80c6038d7b09d91ca
Adam Heath <adam@brainfood.com>
2000-02-06 00:55:07 +0000

    Got a 7.5% speedup.  Still takes longer than I want.

--- modules/also_inherit.sh
+++ modules/also_inherit.sh
@@ -1,5 +1,5 @@
 IFS=" " OBJECT . also_inherit : '
-	eval local a curp=$1 newparents="" parents="" retval=0 lastp _seen_child_$THIS=1;
+	eval local a curp=$1 newparents="" parents="" retval=0 lastp _seen_child_$THIS=1 p;
 	eval set -- $(eval eval "\$_shoop_${THIS}_parent") "$@";
 
 	while [ $# -gt 0 ];do
@@ -7,30 +7,23 @@ IFS=" " OBJECT . also_inherit : '
 			while [ "$1" = "}" ]; do
 				: Pop the stack;
 				eval unset _seen_unset_$2 \$_seen_unset_$2;
-				lastp=$2 curp=$3;
+				lastp=$2;
 				shift 2;
 			done;
-			if eval [ \"\$bad_parent_$lastp\" ]; then
-				retval=1;
-				echo "Parent loop detected. $THIS:$lastp"
-			else
+			curp=$1;
+			if [ -z \"\$bad_parent_$lastp\" ] ; then
 				newparents="$newparents $lastp"
-			fi;
+			fi
 		fi
 		# We have processed this node, so skip it.
 		if eval [ \"\$visited_$1\" ]; then
-			shift
-			continue
-		fi;
-		if eval local visited_$1=1\; [ \"\$_seen_child_$1\" ]; then
+			shift;
+		elif eval [ \"\$_seen_child_$1\" ]; then
 			# Parent Loop : $THIS $curp;
-			local bad_parent_$curp=1;
-			shift;
-			continue
+			local bad_parent_$curp=1 retval=1 visited_$1=1; shift;
+			echo "Parent loop detected. $THIS:$curp"
 		else
-			eval local _seen_child_$1=1 _seen_unset_$curp=\"\$_seen_unset_$curp _seen_child_$1\"\;
-				parents=$(eval eval "\$_shoop_$1_parent") p=$1;
-			shift;
+			eval local visited_$1=1 _seen_unset_$curp=\"\$_seen_unset_$curp _seen_child_$1\" _seen_child_$1=1 parents=$(eval eval "\$_shoop_${1}_parent") p=$1; shift;
 			if [ "$parents" ];then
 				set -- $parents "}" "$p" "$@"
 			fi
@@ -38,8 +31,5 @@ IFS=" " OBJECT . also_inherit : '
 	done;
 	# newparents is $newparents;
 	$THIS . parents .= $newparents
-	echo "newparents is $newparents" >&2;
 	return $retval
 '
-#		eval local visited_$1=1;
-#		if eval [ \"\$_seen_child_$1\" ]; then
