401b94ed55d93009c793b8caa555d42766276824
Adam Heath <adam@brainfood.com>
2001-02-20 07:14:11 +0000

    Added LIST module.  No documentation yet.

---
+++ modules/list.sh
@@ -0,0 +1,51 @@
+# TODO
+# erase variables(requires shoop.sh support)
+
+OBJECT . new LIST
+
+LIST . size =q 0
+LIST . _count =q 0
+LIST . _order =q ''
+
+LIST . add :p '
+	local count=$(($($THIS . _count) + 1))
+	$THIS . _count =q $count
+	$THIS . size =q $(($($THIS . size) + 1))
+	$THIS . _order =q "$($THIS . _order) $count"
+	$THIS . set $count "$@"
+'
+LIST . insert :p '
+	$THIS . insertat 1 "$@"
+'
+LIST . insertat :p '
+	local pos="$1"; shift
+	local count=$(($($THIS . _count) + 1))
+	$THIS . _count =q $count
+	$THIS . size =q $(($($THIS . size) + 1))
+	$THIS . _order =q "$count $($THIS . _order)"
+	$THIS . set $pos "$@"
+'
+LIST . get :p '
+	local pos=$1; shift
+	set -- $($THIS . _order)
+	eval $THIS . _\$$pos
+'
+LIST . set :p '
+	local pos=$1; shift
+	local args="$@"
+	set -- $($THIS . _order)
+	eval $THIS . _\$$pos = \"\$args\"
+'
+LIST . delete :p '
+	local ret left pos=$1 order="$($THIS . _order)"; shift
+	$THIS . get $pos
+	eval $THIS d _\$$pos
+	set -- $order
+	shift $(($pos - 1))
+	left="${order%%$@}"
+	shift
+	$THIS . _order =q "$left $@"
+	$THIS . size =q $(($($THIS . size) - 1))
+	order="$($THIS . _order)"
+	: order $order
+'
---
+++ t/list.sh
@@ -0,0 +1,38 @@
+#!/bin/sh -e
+# Inheritance regression test. Also sets basic methods and variables.
+. ${SHOOPSH:-/usr/bin/shoop.sh}
+. ${SHOOPMOD:-/usr/share/shoop/modules}/use.sh
+
+
+ok "" 0 ""		OBJECT . use list
+
+# setup
+ok "" 0 ""		LIST . new list
+ok "" 0 0		list . size
+
+# addition
+ok "" 0 "first"		list . add "first"
+ok "" 0 1		list . size
+ok "" 0 "second"	list . add "second"
+ok "" 0 2		list . size
+
+# insertion
+ok "" 0 "third"		list . insert "third"
+ok "" 0 3		list . size
+
+# fetching
+ok "" 0 "first"		list . get 2
+ok "" 0 "second"	list . get 3
+
+# setting
+ok "" 0 "FIRST"		list . set 2 "FIRST"
+ok "" 0 "FIRST"		list . get 2
+
+# deletion
+ok "" 0 "FIRST"		list . delete 2
+ok "" 0 2		list . size
+ok "" 0 "second"	list . get 2
+ok "" 0 "SECOND"	list . add "SECOND"
+ok "" 0 3		list . size
+
+tests 18
